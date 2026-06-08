d4d3f0deb5b92daf66f7c38f5ea3884091705fd5
astro-okymauludany <oky.mauludany@astronauts.id>
2026-06-08 16:11:02 +0700

    update

--- js/config.js
+++ js/config.js
@@ -26,28 +26,56 @@ const CONFIG = {
 
   // --- Minimum Duration per Sport (in minutes) ---
   MIN_DURATION: {
-    'Run':          20,
-    'Walk':         20,
-    'Ride':         30,
-    'Cycling':      30,
-    'VirtualRide':  30,
-    'Swim':         20,
-    'Padel':        45,
-    'Tennis':       45,
-    'Badminton':    30,
-    'WeightTraining': 30,
-    'Workout':      30,
-    'Yoga':         30,
-    'Pilates':      30,
-    'Hike':         30,
-    'Basketball':   30,
-    'Soccer':       45,
-    'Rowing':       20,
-    'CrossFit':     30,
-    'Elliptical':   20,
-    'StairStepper': 20,
-    'Boxing':       30,
-    'DEFAULT':      30,
+    // Running
+    'Road Running':           20,
+    'Trail Running':          30,
+    'Track Running':          20,
+    'Treadmill Running':      20,
+    'Virtual Running':        20,
+    // Cycling
+    'Road Cycling':           30,
+    'Mountain Biking (MTB)':  30,
+    'Gravel Cycling':         30,
+    'Indoor Cycling':         30,
+    'eBike':                  30,
+    // Swimming
+    'Pool Swimming':          20,
+    'Open Water Swimming':    20,
+    // Triathlon & Multisport
+    'Triathlon':              45,
+    // Hiking & Outdoor
+    'Hiking':                 30,
+    'Walking':                20,
+    'Climbing':               30,
+    // Gym & Fitness
+    'Strength Training':      30,
+    'HIIT':                   20,
+    'Cardio':                 20,
+    'Yoga':                   30,
+    'Pilates':                30,
+    'Elliptical':             20,
+    'Stair Stepper':          20,
+    'Indoor Rowing':          20,
+    // Paddling
+    'Rowing':                 20,
+    'Kayaking':               20,
+    'Stand-Up Paddleboarding (SUP)': 20,
+    // Racket Sports
+    'Badminton':              30,
+    'Tennis':                 45,
+    'Padel':                  45,
+    'Table Tennis':           30,
+    // Team Sports
+    'Basketball':             30,
+    'Volleyball':             30,
+    'Soccer/Football':        45,
+    'Futsal':                 45,
+    // Martial Arts
+    'Boxing':                 30,
+    'Martial Arts':           30,
+    // Golf
+    'Golf':                   60,
+    'DEFAULT':                30,
   },
  
   // --- Scoring System ---
